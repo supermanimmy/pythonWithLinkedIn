895e74160d7ad6f74edfc8bb4f40c601960812ef
Imran <58345725+supermanimmy@users.noreply.github.com>
2021-07-07 23:44:35 +0100

    Constructing a class
    Working with python class

---
+++ Python Essential Training/Classes/constructor-working.py
@@ -0,0 +1,30 @@
+"""Constructing an object"""
+
+class Animal:
+    def __init__(self, type, name, sound):
+        self._type=type
+        self._name = name
+        self._sound = sound
+
+    def type(self):
+        return self._type
+    
+    def name(self):
+        return self._name
+
+    def sound(self):
+        return self._sound
+
+def print_animal(o):
+    if not isinstance(o, Animal):
+        raise TypeError('print_animal(): requires an Animal.')
+    print('The {} is named "{}" and says "{}".'.format(o.type(), o.name(), o.sound()))
+
+def main():
+    a0 = Animal('Kitten', 'Meow', 'Meowww')
+    a1 = Animal('duck', 'Donald', 'QUACK')
+
+    print_animal(a0)
+
+if __name__ == '__main__':
+    main()
