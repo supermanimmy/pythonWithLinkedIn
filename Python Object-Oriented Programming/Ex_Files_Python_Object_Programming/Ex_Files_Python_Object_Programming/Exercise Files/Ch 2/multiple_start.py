a0a0f9d8f1e72fd7b89d562d0a07ad8dabf9b584
Imran Shah <imranshah0406@gmail.com>
2021-10-22 07:34:12 +0100

    Update

--- Python Object-Oriented Programming/Ex_Files_Python_Object_Programming/Ex_Files_Python_Object_Programming/Exercise Files/Ch 2/multiple_start.py
+++ Python Object-Oriented Programming/Ex_Files_Python_Object_Programming/Ex_Files_Python_Object_Programming/Exercise Files/Ch 2/multiple_start.py
@@ -6,17 +6,26 @@ class A:
     def __init__(self):
         super().__init__()
         self.foo = "foo"
+        self.bar= 'abar'
 
 
 class B:
     def __init__(self):
         super().__init__()
         self.bar = "bar"
+        self.foo='bfoo'
 
 
-class C(A, B):
+#Order of classes matter if both classes have similiar variables
+class C(B, A):
     def __init__(self):
         super().__init__()
 
+    def showprops(self):
+        print(self.foo)
+        print(self.bar)
+
 
 c = C()
+c.showprops()
+print(C.__mro__)
